0da240af711721b9d222bf33d9c134d6088788c7
mingfang0318 <mingfang0318@hotmail.com>
2022-01-15 21:32:01 +0100

    test sur docker nginx

--- src/main/java/testConfig.java
+++ src/main/java/testConfig.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class testConfig {
     public static void main(String[] args) throws Exception {
-        File f = new File("source.htm");
+        File f = new File("filesHtmls/source.html");
         BufferedWriter bw = new BufferedWriter(new FileWriter(f));
         bw.write("<html><body><h1>Blah, Blah!</h1>");
         bw.write("<textarea cols=75 rows=10>");
