3584067859b6d9fc090d827bdb13775cc4f6b018
mingfang0318 <mingfang0318@hotmail.com>
2022-01-24 09:03:37 +0100

    CI

--- src/main/java/testConfig.java
+++ src/main/java/testConfig.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class testConfig {
     public static void main(String[] args) throws Exception {
-        File f = new File("filesHtmls/source.html");
+        File f = new File("docker-nginx/data/source.html");
         BufferedWriter bw = new BufferedWriter(new FileWriter(f));
         bw.write("<html><body><h1>Blah, Blah!</h1>");
         bw.write("<textarea cols=75 rows=10>");
