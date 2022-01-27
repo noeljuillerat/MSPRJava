d6ddabc1006d6235da154fe13a29f0ea367652db
mingfang0318 <mingfang0318@hotmail.com>
2022-01-27 20:25:37 +0100

    junit

--- src/main/java/testConfig.java
+++ src/main/java/testConfig.java
@@ -3,27 +3,22 @@ package main.java;
 import java.awt.Desktop;
 import java.io.*;
 
-public class testConfig {
+public class TestConfig {
     public static void main(String[] args) throws Exception {
         File f = new File("docker-nginx/data/source.html");
-        BufferedWriter bw = new BufferedWriter(new FileWriter(f));
-        bw.write("<html><body><h1>Blah, Blah!</h1>");
-        bw.write("<textarea cols=75 rows=10>");
-        for (int ii=0; ii<20; ii++) {
-            bw.write("Blah blah..");
+        try (
+                BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+        ) {
+            bw.write("<html><body><h1>Blah, Blah!</h1>");
+            bw.write("<textarea cols=75 rows=10>");
+            for (int ii=0; ii<20; ii++) {
+                bw.write("Blah blah..");
+            }
+            bw.write("</textarea>");
+            bw.write("</body></html>");
+        } catch(IOException e) {
+            //log or propagate to the caller
         }
-        bw.write("</textarea>");
-        bw.write("</body></html>");
-        bw.close();
-
         Desktop.getDesktop().browse(f.toURI());
-
-        //test
-        add(10, 20);
-
-
-    }
-    public static int add(int a, int b){
-        return  a + b;
     }
 }
