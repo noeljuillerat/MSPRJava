47a82222c02ba433105058b4d431be9bc93c8a40
mingfang0318 <mingfang0318@hotmail.com>
2022-01-28 17:09:06 +0100

    bufferreader get staff data

--- src/main/java/testConfig.java
+++ src/main/java/testConfig.java
@@ -2,15 +2,44 @@ package main.java;
 
 import java.awt.Desktop;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class testConfig {
     public static void main(String[] args) throws Exception {
-        readFile();
+        List<String> listStaff = new ArrayList<>();
+        listStaff = readFileGetStaff("Donnees/staff.txt");
+        getStaffData(listStaff);
         writePage();
 
     }
 
-    public  static void writePage () throws IOException {
+    public static void getStaffData(List<String> listStaff) throws IOException {
+        List<Staff> listObjetStaff = new ArrayList<>();
+
+        if(listStaff != null) {
+            for (int i=0; i<listStaff.size(); i++){
+                //get all data of a staff
+                String staffNom = listStaff.get(i);
+                List<String> listInfo = readFileGetStaff("Donnees/" + staffNom + "/" + staffNom + ".txt");
+                System.out.println("Donnees/" + staffNom + "/" + staffNom + ".txt");
+                Staff stafObjet = new Staff();
+                stafObjet.nom = listInfo.get(0);
+                stafObjet.prenom = listInfo.get(1);
+                stafObjet.misson = listInfo.get(2);
+                stafObjet.motDePasse= listInfo.get(3);
+                stafObjet.affecMater = new ArrayList<>();
+                for (int j=5; j<listInfo.size(); j++){
+                    stafObjet.affecMater.add(listInfo.get(j));
+                    //System.out.println(listInfo.get(j));
+                }
+                listObjetStaff.add(stafObjet);
+            }
+        }
+
+    }
+
+    public static void writePage () throws IOException {
         File f = new File("docker-nginx/data/source.html");
         try (
             BufferedWriter bw = new BufferedWriter(new FileWriter(f));
@@ -25,26 +54,31 @@ public class testConfig {
         } catch(IOException e) {
             //log or propagate to the caller
         }
-        Desktop.getDesktop().browse(f.toURI());
-
+        //Desktop.getDesktop().browse(f.toURI());
     }
 
-    public  static void readFile () throws IOException {
+    public static List<String> readFileGetStaff (String path) throws IOException {
         Reader reader = null;
+        List<String> listStaff = new ArrayList<>();
         try {
-            reader = new FileReader("staff.txt");
+            reader = new FileReader(path);
+            // Create a BufferedReader with buffer array size of 16384 (32786 bytes = 32 KB).
+            BufferedReader br = new BufferedReader(reader, 16384);
+            for(String line=br.readLine(); line!=null; line=br.readLine())
+            {
+                listStaff.add(line);
+                System.out.println(line);
+            }
+            String line = null;
+            while((line=br.readLine())!=null)
+            {
+                listStaff.add(line);
+                System.out.println(line);
+            }
+            //br.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        // Create a BufferedReader with buffer array size of 16384 (32786 bytes = 32 KB).
-        BufferedReader br = new BufferedReader(reader, 16384);
-
-        String line = null;
-
-        while((line = br.readLine())!= null) {
-            System.out.println(line);
-        }
-        br.close();
+        return listStaff;
     }
 }
