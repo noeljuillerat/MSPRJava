d8189736f3a2feb9fe7bdc269dc691a69ce32e52
mingfang0318 <mingfang0318@hotmail.com>
2022-01-28 17:09:21 +0100

    bufferreader get staff data

---
+++ src/main/java/Staff.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import java.util.List;
+
+public class Staff {
+    String nom;
+    String prenom;
+    String misson;
+    String motDePasse;
+    List<String> affecMater;
+
+}
